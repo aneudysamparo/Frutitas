11b57dd8667ef78cd6a5638c46e6d54ec85373c8
Aneudys Amparo <aneudysamparo@gmail.com>
2024-02-14 17:49:31 -0400

    Initial Commit

---
+++ Frutitas/FrutitasApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrutitasApp.swift
+//  Frutitas
+//
+//  Created by Aneudys Amparo on 14/2/24.
+//
+
+import SwiftUI
+
+@main
+struct FrutitasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
